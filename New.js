e7783c2d38c6fa0f24c3c8fe46aeee413338f173
kevin19-code <Keurydaviel91@gmail.com>
2025-01-29 23:24:12 -0400

    Add files via upload

---
+++ New.js
@@ -0,0 +1,28 @@
+var boton = document.getElementById("Boton")
+var text = document.getElementById('tectonica')
+
+
+function hola(){
+
+boton.style.backgroundColor='rgb(50,50,200)'
+text.textContent=('Su formulario ha sido enviado')
+
+}
+
+function jota(){
+
+boton.style.backgroundColor='blue'
+
+}
+
+function jola(){
+
+    boton.style.backgroundColor='aqua'
+    
+    }
+    
+
+
+
+
+
